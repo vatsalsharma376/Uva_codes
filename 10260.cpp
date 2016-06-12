f89949c16f5dc9c7cfd5a16bf3181ee0d1cb3dc2
Vatsal Sharma <vatsalsharma376@gmail.com>
2016-06-12 10:20:33 +0530

    Update 10260.cpp

--- 10260.cpp
+++ 10260.cpp
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
-    using namespace std;
-     
-    int main() {
-    	int t,n;
-    	cin>>t;
-    	getchar();
-    	getchar();
-    	int p=t;
-    	while(t--){
-    	map<string,int> a;//no of tree
-    	set<string> k;//list of trees
-    		string s1;
-    	set<string>::iterator it;
-    		n=0;
-    		while(getline(cin,s1)){
-    			if(s1.length()==0) {break;}
-    			a[s1]++;
-    			++n;
-    			k.insert(s1);
-    		}
-    		for(it=k.begin();it!=k.end();it++){
-    			cout<<*it<<" ";
-    			printf("%.4f\n",100.00*a[*it]/n);
-    		}
-    		--p;
-    		
-    		if(p!=0)cout<<"\n";
-    	}	
+using namespace std;
+
+int main() {
+	string s;
+int x1=-1,x2;
+	 while(cin>>s){
+        x1=-1;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='C'||s[i]=='G'||s[i]=='J'||s[i]=='K'||s[i]=='Q'||s[i]=='S'||s[i]=='X'||s[i]=='Z') x2=2;
+            else if(s[i]=='B'||s[i]=='F'||s[i]=='P'||s[i]=='V') x2=1;
+            else if(s[i]=='D'||s[i]=='T') x2=3;
+            else if(s[i]=='M'||s[i]=='N') x2=5;
+            else if(s[i]=='L') x2=4;
+            else if(s[i]=='R') x2=6;
+            else x2=-1;
+            
+            if(x2!=x1 && x2!=-1) cout<<x2;
+            
+            x1=x2;
+        }
+        cout<<endl;        
     }
+}
