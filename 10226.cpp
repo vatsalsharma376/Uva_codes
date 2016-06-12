6f517268a0dfa57baaa15a09bc605c7b5c204547
Vatsal Sharma <vatsalsharma376@gmail.com>
2016-06-12 10:19:33 +0530

    Update 10226.cpp

--- 10226.cpp
+++ 10226.cpp
@@ -1,39 +1,30 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-	int ld, nRec;
-	double dp, loan, decP[101];
-
-	while (true) {
-		cin >> ld >> dp >> loan >> nRec;
-		if (ld < 0)
-			break;
-
-		int m;
-		double p;
-		while (nRec--) {
-			cin >> m >> p;
-			for (int i = m; i < 101; i++)
-				decP[i] = p;
-		}
-
-		int now = 0;
-		double mp = loan / ld;
-		double curVal = (loan + dp) * (1 - decP[0]);
-		double curLoan = loan;
-		while (curVal < curLoan) {
-			now++;
-			curLoan -= mp;
-			curVal = curVal * (1-decP[now]);
-		}
-
-		cout << now << " month";
-		if (now != 1)
-			cout << "s";
-		cout << endl;
-	}
-
-	return 0;
-}
+#include <bits/stdc++.h>
+    using namespace std;
+     
+    int main() {
+    	int t,n;
+    	cin>>t;
+    	getchar();
+    	getchar();
+    	int p=t;
+    	while(t--){
+    	map<string,int> a;//no of tree
+    	set<string> k;//list of trees
+    		string s1;
+    	set<string>::iterator it;
+    		n=0;
+    		while(getline(cin,s1)){
+    			if(s1.length()==0) {break;}
+    			a[s1]++;
+    			++n;
+    			k.insert(s1);
+    		}
+    		for(it=k.begin();it!=k.end();it++){
+    			cout<<*it<<" ";
+    			printf("%.4f\n",100.00*a[*it]/n);
+    		}
+    		--p;
+    		
+    		if(p!=0)cout<<"\n";
+    	}	
+    }
