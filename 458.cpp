7725ab5db24f3edc08d227ccfa7ebf1c953806e0
Vatsal Sharma <vatsalsharma376@gmail.com>
2016-06-12 10:33:44 +0530

    Create 458.cpp

---
+++ 458.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x);
+#define in(y) insert(y);
+#define tt(t) while(t--)
+#define itr ::iterator it;
+#define ll long long
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+#define si set<int>                      
+#define msi map<string, int>
+#define new cout<<endl
+#define ff(n) for(int i=0;i<n;i++)
+#define all(v) sort(v.begin(),v.end())
+
+using namespace std;
+
+int main(){
+    string s;
+    int b;
+    char c;
+    while(cin>>s){
+        ff(s.size()){
+            b=s[i]-7;
+            c=b;
+            cout<<c;
+        } 
+        cout<<endl;
+        
+    }
+    return 0;
+}
