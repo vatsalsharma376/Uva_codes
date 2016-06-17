9e436aa141cf5cdf58e73fd883818ceb049c0da9
Vatsal Sharma <vatsalsharma376@gmail.com>
2016-06-17 21:13:04 +0530

    Create 12640.cpp

---
+++ 12640.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define vi vector<int> 
+#define pb push_back
+#define ff(x,y) for(int i=x;i<y;i++)
+#define ffr(x,y) for(int i=x;i<=y;i++)     
+#define getcx getchar_unlocked
+#define pcx putchar_unlocked            
+#define MX INT_MAX
+
+
+int sub(vector<int> a , int size)
+{
+   int max_so_far = 0, max_ending_here = 0;
+   for (int i = 0; i < size; i++)
+   {
+       max_ending_here = max_ending_here + a[i];
+       if (max_ending_here < 0)
+           max_ending_here = 0;
+
+       else if (max_so_far < max_ending_here)
+           max_so_far = max_ending_here;
+   }
+   return max_so_far;
+}
+
+int main(){
+    string s;
+    while(getline(cin,s)){
+        stringstream ss(s);
+        vector<int> a;
+        int x;
+        while(ss>>x) a.pb(x);
+        printf("%d\n",sub(a,a.size()));
+    }
+}
